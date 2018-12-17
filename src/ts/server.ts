6472109988004c1892ad2e474836e4e9f3d1f088
8eecf0d2 <8eecf0d23d248b0e6223b8a10ddbf1@gmail.com>
2018-12-18 09:07:10 +1100

    💫 added simple plugin & dumb event emitter system

--- src/ts/server.ts
+++ src/ts/server.ts
@@ -11,6 +11,7 @@ export class Server {
 
   private servers: Server.Servers = {};
   private router: Router;
+  private events: { [key: string]: Server.Event.Handler[] } = {};
 
   constructor(
     private options: Server.Options,
@@ -29,44 +30,81 @@ export class Server {
 
   public start(): void {
     if (this.servers.http) {
-      this.servers.http.listen(this.options.http, () => Logger.good(`[http]:  http://localhost:${this.options.http}`));
+      this.servers.http.listen(this.options.http, () => {
+        this.emit("start", "http");
+        Logger.good(`[http]: http://localhost:${this.options.http}`);
+      });
     }
     if (this.servers.https) {
-      this.servers.https.listen(this.options.https, () => Logger.good(`[https]: https://localhost:${this.options.https}`));
+      this.servers.https.listen(this.options.https, () => {
+        this.emit("start", "https");
+        Logger.good(`[https]: https://localhost:${this.options.https}`);
+      });
     }
   }
 
   public stop(): void {
     if (this.servers.http) {
-      this.servers.http.close(() => Logger.good(`http : stopped`));
+      this.servers.http.close(() => {
+        this.emit("stop", "http");
+        Logger.good(`[http]: stopped`);
+      });
     }
     if (this.servers.https) {
-      this.servers.https.close(() => Logger.good(`https: stopped`));
+      this.servers.https.close(() => {
+        this.emit("stop", "https");
+        Logger.good(`[https]: stopped`);
+      });
     }
   }
 
   private http(): void {
     this.servers.http = http.createServer();
-    this.servers.http.on("request", this.router.handle.bind(this.router));
+    this.servers.http.on("request", (request, response) => {
+      this.emit("request", { request, response });
+      this.router.handle(request, response);
+    });
   }
 
   private https(): void {
     this.servers.https = https.createServer(this.options.options);
-    this.servers.https.on("request", this.router.handle.bind(this.router));
+    this.servers.https.on("request", (request, response) => {
+      this.emit("request", { request, response });
+      this.router.handle(request, response);
+    });
   }
 
-  public plugin (plugin: Server.Plugin): void {
-    Logger.good(`plugin: ${plugin.name}`);
-    for (const route of plugin.routes) {
-      this.router.register(route);
+  public async plugin(plugin: Server.Plugin): Promise<void> {
+    Logger.good(`[plugin]: "${plugin.name}"`);
+    const instance = new plugin(this);
+  }
+
+  public on(event: Server.Event.Type, handler: Server.Event.Handler): void {
+    if(!this.events.hasOwnProperty(event)) {
+      this.events[event] = [];
+    }
+
+    this.events[event].push(handler);
+  }
+
+  public emit(event: Server.Event.Type, payload: any): void {
+    if(!this.events.hasOwnProperty(event)) {
+      return;
+    }
+
+    for(const handler of this.events[event]) {
+      handler(payload);
     }
   }
 }
 
 export namespace Server {
   export interface Plugin {
-    name: string;
-    routes: Router.Route[];
+    new(server: Server): any;
+  }
+  export namespace Event {
+    export type Type = "start" | "stop" | "request";
+    export type Handler = (...args: any[]) => any;
   }
   export interface Options {
     routes: Router.Route[];
